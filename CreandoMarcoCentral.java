80cc22165c896a974cc6f463c52999c6c3b41f05
Espadaclar <eneroborde8002@gmail.com>
2017-06-19 00:22:17 +0200

    video_57, en Apli_2, cambia el icono de java por uno elegido.

--- CreandoMarcoCentral.java
+++ CreandoMarcoCentral.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Image;
 /**
  * Averigua el tamano de la pantalla, y crea una ventana de acuerdo a estos datos.
  */
@@ -27,7 +28,14 @@ public class CreandoMarcoCentral extends JFrame
         setSize(largoPantalla/2, alturaPantalla/2);
         //para centrarla, el punto 0/0 de la ventana seria 1/4 de la pantalla.
         setLocation(largoPantalla/4, alturaPantalla/4);
-
+        setTitle("Marco centrado");
+        
+        //PARA CAMBIAR EL ICONO DE JAVA QUE APARECE EN LA ESQUINA IZQUIERDA.
+        //actuamos sobre 'miPantalla' por tener todos los datos de la pantalla.
+        //1º conseguimos el icono y se almacena en un objeto Image
+        Image miIcono = miPantalla.getImage("icono2.gif");
+        //Le decimos que establezca este icono.
+        setIconImage(miIcono);
     }
 
     /**

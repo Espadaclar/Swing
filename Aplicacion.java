98201e2099be001609e9671063a696bc84fa538b
Espadaclar <eneroborde8002@gmail.com>
2017-06-18 10:25:31 +0200

    Codigo para crear una ventana en la pantalla.

---
+++ Aplicacion.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+import javax.swing.*;
+/**
+ * @version 17 / 06 / 2017)
+ */
+public class Aplicacion
+{
+    public static void main(String[] args){
+        MiMarco marco1 = new MiMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ MiMarco.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+/**
+ * @version 17 / 06 / 2017)
+ */
+public class MiMarco extends JFrame
+{
+    public MiMarco(){
+        setSize(500, 300);
+    }
+    
+}
